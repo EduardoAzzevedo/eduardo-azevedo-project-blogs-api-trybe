1d278f5a435f2fc8f98a8257c3e7bd70176fd444
Eduardo Azevedo <eduardo-azevedo@outlook.com>
2022-10-13 19:32:32 -0300

    Insere autenticação de token.

--- src/controllers/categoryController.js
+++ src/controllers/categoryController.js
@@ -17,14 +17,15 @@ const insertCategory = async (req, res) => {
 };
 
 const getAllCategories = async (req, res) => {
+  const { authorization } = req.headers;
+  if (!authorization) return res.status(401).json({ message: 'Token not found' });
+
   try {
+    createToken.tokenVerify(authorization);
     const categories = await categoryService.getAllCategories();
-    if (!categories) {
-      return res.status(404).json({ message: 'Not found' });
-    }
     return res.status(200).json(categories);
   } catch (e) {
-    return res.status(500).json({ message: e.message });
+    return res.status(401).json({ message: 'Expired or invalid token' });
   }
 };
 
